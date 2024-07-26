88ee69b2361b7bac3be7c8ef9aac3b90004df50a
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2024-07-27 01:24:07 +0300

    Rm binary files

--- codeforces/build.sh
+++ codeforces/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 g++ -Wall -Wextra -pedantic -std=c++20 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op \
     -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC \
-    -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector -I /home/ivan/cs/contests/codeforces/include $1 -o main
+    -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector -I /home/ivan/cs/contests/codeforces/include $1 
